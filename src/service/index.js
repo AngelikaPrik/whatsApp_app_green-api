e6fa10f2801e954dd04db6856377f9429119722d
Angelina Mamakova <pretzel318@gmail.com>
2023-06-13 15:12:04 +0400

    minor changes

--- src/service/index.js
+++ src/service/index.js
@@ -6,7 +6,7 @@ export const getAuth = ({ id, apiToken }) => {
   return axios.get(`${BASE_URL}/waInstance${id}/getStateInstance/${apiToken}`)
 }
 
-export const sendMessage = async data => {
+export const sendMessage = data => {
   const { account, textMessage } = data
 
   const url = `${BASE_URL}/waInstance${account.id}/sendMessage/${account.apiToken}`
@@ -16,13 +16,13 @@ export const sendMessage = async data => {
   })
 }
 
-export const receiveNotification = async data => {
+export const receiveNotification = data => {
   const { id, apiToken } = data
   const url = `${BASE_URL}/waInstance${id}/receiveNotification/${apiToken}`
   return axios.get(url)
 }
 
-export const deleteNotification = async data => {
+export const deleteNotification = data => {
   const { account, receiptId } = data
   const url = `${BASE_URL}/waInstance${account.id}/deleteNotification/${account.apiToken}/${receiptId}`
   return axios.delete(url)
